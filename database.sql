7100140342d0cab748cd485aa7aaabf31e8a68c4
jacobholtz <jacob.holtz20@gmail.com>
2022-01-31 19:46:34 -0600

    added potential data keywords

--- database.sql
+++ database.sql
@@ -3,24 +3,32 @@ USE ipro_db;
 DROP TABLE processes;
 
 CREATE TABLE processes(
-    ID INT NOT NULL AUTO_INCREMENT ,
+    ID INT NOT NULL AUTO_INCREMENT,
     PROCESS_ID INT NOT NULL,
+    FILE_PATH TEXT,
+    MEM_USAGE INT,
+    CPU_USAGE INT,
     SESSION_NAME TEXT,
-    SESSION_NUMBER TEXT,
-    MEM_USAGE TEXT,
+    OPEN_FILES TEXT,
     PROCESS_NAME TEXT,
     TIME_STAMP TEXT,
+    UIDS INT,
+    USERNAME TEXT,
     PRIMARY KEY (ID)
 );
 
-INSERT INTO processes (PROCESS_ID, SESSION_NAME, SESSION_NUMBER, MEM_USAGE, PROCESS_NAME, TIME_STAMP) VALUES(
-	-- example data, later will need to get data formatted from .csv file from front-end
+INSERT INTO processes (PROCESS_ID, FILE_PATH, MEM_USAGE, CPU_USAGE, SESSION_NAME, OPEN_FILES, PROCESS_NAME, TIME_STAMP, UIDS, USERNAME) VALUES(
+	-- example data, later will need to get data from front-end
     500,
-    "CONSOLE",
-    "1",
-    "500kb",
-    "TEST.EXE",
-    "1/24/2022"
+    "C:/Windows/System32/virus.exe",
+    75,
+    50,
+    "virus.exe",
+    "C:/Users/Brody/Desktop/secret_information.txt, C:/Users/Administrator/Desktop/enterprise_login_information.txt",
+    "virus.exe",
+    "07:40:39 1/31/2022",
+    3,
+    "administrator"
 );
 
 SELECT * FROM processes;
